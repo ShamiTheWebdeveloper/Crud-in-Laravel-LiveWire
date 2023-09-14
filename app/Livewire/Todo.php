994c18a8bdb5f5ac05ba41e2fb7f0ec2fef9a232
azhar <azhar@enterpristore.com>
2023-09-14 22:49:36 +0500

    first commit

--- app/Livewire/Todo.php
+++ app/Livewire/Todo.php
@@ -6,10 +6,39 @@ use Livewire\Component;
 use App\Models\User;
 class Todo extends Component
 {
-    public $users;
+    public $users,$name,$email,$password,$user_id,$num=1,$show_edit_modal=false;
+
     public function render()
     {
         $this->users=User::all();
+        $this->num;
         return view('livewire.todo');
+    }
+    public function show_edit_modal(){
+        $this->show_edit_modal=true;
+    }
+    public function store(){
+        $validate=$this->validate([
+            'name'=>'required',
+            'email'=>'required',
+            'password'=>'required'
+        ]);
+        User::create($validate);
+        session()->flash('message', 'User Created Successfully.');
+    }
+    public function edit($id){
+        $user_edit=User::findorfail($id);
+        $this->user_id=$id;
+        $this->name=$user_edit->name;
+        $this->email=$user_edit->email;
+    }
+    public function resetInputFields(){
+        $this->name = '';
+        $this->email = '';
+        $this->password='';
+    }
+    public function delete($id){
+        User::destroy($id);
+        session()->flash('message', 'User Deleted Successfully.');
     }
 }
--- resources/views/livewire/todo.blade.php
+++ resources/views/livewire/todo.blade.php
@@ -2,7 +2,14 @@
 <div class="m-4">
     <h2 class="text-center">Crud in Laravel LiveWire</h2>
 </div>
-    <div class="text-end"><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#edit-and-add">Add User</button></div>
+    <div class="text-end"><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#add-user">Add User</button></div>
+    @if (session()->has('message'))
+        <div class="message alert alert-success my-2 p-1">
+            <p class="m-1">
+                {{ session('message') }}
+            </p>
+        </div>
+    @endif
     <table class="table table-hover">
         <thead>
         <tr>
@@ -16,14 +23,14 @@
         @forelse($users as $user)
         <tbody>
         <tr>
-            <td>{{ $user->id }}</td>
+            <td>{{ $num++ }}</td>
             <td>{{ $user->name }}</td>
             <td>{{ $user->email }}</td>
             <td>{{ $user->created_at }}</td>
             <td>
-                <button class="btn btn-primary">Edit</button>
+                <button wire:click='edit({{ $user->id }})' class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#edit-user">Edit</button>
                 &nbsp;
-                <button class="btn btn-danger">Delete</button>
+                <button wire:click='delete({{ $user->id }})' class="btn btn-danger">Delete</button>
             </td>
         </tr>
         </tbody>
@@ -31,21 +38,69 @@
         <p class="text-danger text-center">No users yet</p>
         @endforelse
     </table>
-    {{--Modal--}}
-    <div class="modal fade" id="edit-and-add" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    {{--Add Modal--}}
+    <div class="modal fade" id="add-user"  tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Add User</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
+                <form>
                 <div class="modal-body">
-                    ...
+                        <div class="form-group">
+                            <label>Name:</label>
+                            <input type="text" class="form-control" wire:model="name">
+                            @error('name') <span class="text-danger">{{ $message }}</span>@enderror
+                        </div>
+                        <div class="form-group my-2">
+                            <label>Email:</label>
+                            <input type="email" class="form-control" wire:model="email">
+                            @error('email') <span class="text-danger">{{ $message }}</span>@enderror
+                        </div>
+                        <div class="form-group">
+                            <label>Password:</label>
+                            <input type="password" wire:model="password" class="form-control">
+                            @error('password') <span class="text-danger">{{ $message }}</span>@enderror
+                        </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="submit" class="btn btn-primary" wire:click.prevent="store()" data-bs-dismiss="modal" aria-label="Close">Save changes</button>
                 </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    {{--  Edit modal  --}}
+    <div class="modal fade" id="edit-user" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="">Edit User</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" wire:click.prevent="resetInputFields()"></button>
+                </div>
+                <form>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label>Name:</label>
+                            <input type="text" class="form-control" wire:model="name">
+                            @error('name') <span class="text-danger">{{ $message }}</span>@enderror
+                        </div>
+                        <div class="form-group my-2">
+                            <label>Email:</label>
+                            <input type="email" class="form-control" wire:model="email">
+                            @error('email') <span class="text-danger">{{ $message }}</span>@enderror
+                        </div>
+                        <div class="form-group">
+                            <label>Password:</label>
+                            <input type="password"  class="form-control">
+                            @error('password') <span class="text-danger">{{ $message }}</span>@enderror
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary" data-bs-dismiss="modal" aria-label="Close">Save changes</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
